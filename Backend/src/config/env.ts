14ef335fd6c3a1d9ed8cb5cb7f03b084718fa466
Prince Gupta <pg875029436655@gmail.com>
2025-02-15 21:02:42 +0530

    minor changes

--- Backend/src/config/env.ts
+++ Backend/src/config/env.ts
@@ -1,6 +1,6 @@
 import { config } from "dotenv";
 
-config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
+config({ path: `.env.${process.env.NODE_ENV}` });
 
 export const { PORT, JWT_SECRET, JWT_EXPIRY, MONGODB_URI, DOCUMENT_NAME } =
   process.env;
